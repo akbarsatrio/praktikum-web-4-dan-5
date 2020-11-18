f1a26edc367b0cbcc536c5285a60bbb135250d93
akbarsatrio <akbarsatrio@outlook.co.id>
2020-11-18 14:02:19 +0700

    AKBARSATRIOYUDHOHARTONO_0110220261_TI07

--- kurs/js/kurs.js
+++ kurs/js/kurs.js
@@ -1,29 +1,29 @@
 function hitung() {
-  let inp = document.getElementById("inp").value;
-  let valas = document.getElementById("valas").value;
-  if(valas == 1) {
-    nilai = 9915;
-    res = inp*nilai;
-    document.getElementById("res").value = res;
-  } else if(valas == 2) {
+  let inp = document.getElementById("inp").value; //ambil value dari id inp
+  let valas = document.getElementById("valas").value; //ambil value dari id valas
+  if(valas == "usd") {  //jika valuenya USD, maka
+    nilai = 9915; //buat nilai per 1 valasnya sebesar Rp. 9915
+    res = inp*nilai; //perkalian apa yang di input user dikali dengan nilai per 1valas
+    document.getElementById("res").value = "Rp. " + res; //tampilkan output ke id res
+  } else if(valas == "sgd") {
     nilai = 13472;
     res = inp*nilai;
-    document.getElementById("res").value = res;
-  } else if(valas == 3) {
+    document.getElementById("res").value = "Rp. " + res;
+  } else if(valas == "myr") {
     nilai = 874;
     res = inp*nilai;
-    document.getElementById("res").value = res;
-  } else if(valas == 4) {
+    document.getElementById("res").value = "Rp. " + res;
+  } else if(valas == "jpy") {
     nilai = 120;
     res = inp*nilai;
-    document.getElementById("res").value = res;
-  } else if(valas == 5) {
+    document.getElementById("res").value = "Rp. " + res;
+  } else if(valas == "eur") {
     nilai = 15888;
     res = inp*nilai;
-    document.getElementById("res").value = res;
-  } else if(valas == 6) {
+    document.getElementById("res").value = "Rp. " + res;
+  } else if(valas == "ryl") {
     nilai = 3592;
     res = inp*nilai;
-    document.getElementById("res").value = res;
+    document.getElementById("res").value = "Rp. " + res;
   }
 }
--- login/js/otentikasi.js
+++ login/js/otentikasi.js
@@ -1,9 +1,9 @@
 function run() {
-  let us = document.getElementById("us").value;
-  let pw = document.getElementById("pw").value;
-  if  (us == "admin" && pw == "admin") {
-      window.alert("Login Berhasil")
-      location.href = "sukses.html";
+  let us = document.getElementById("us").value; //ambil value username di id us
+  let pw = document.getElementById("pw").value; //ambil value password di id pw
+  if  (us == "admin" && pw == "admin") {  //jika username admin dan password admin, jalakan statement true
+      window.alert("Login Berhasil")  //tampilkan alert
+      location.href = "sukses.html";  //lempar ke sukses.html
   } else {
     window.alert("Login Gagal")
     return false;
